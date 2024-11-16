17d0621ab63b67af78f57dba30c9d1bfd464f847
schtrudl <125376055+schtrudl@users.noreply.github.com>
2024-11-16 08:58:31 +0100

    Typify more stuff
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- engine/core/Primitive.js
+++ engine/core/Primitive.js
@@ -1,4 +1,9 @@
 export class Primitive {
+    /**
+     * @param {Object} [param0={}]
+     * @param {import('./Mesh.js').Mesh} param0.mesh
+     * @param {import('./Material.js').Material} param0.material
+     */
     constructor({ mesh, material } = {}) {
         this.mesh = mesh;
         this.material = material;
