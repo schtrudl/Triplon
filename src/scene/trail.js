c8b601c34ed5fa6bd6de1178a000053092c0ca99
BunnyBoy66 <gluvic.samo@gmail.com>
2024-11-25 14:50:01 +0100

    logika za mesh, todo: izris tezava z bufferjem

--- src/scene/trail.js
+++ src/scene/trail.js
@@ -1,4 +1,5 @@
 // @ts-check
+import {vec3} from "../../extern/glm/index.js"
 import {
     Node,
     Model,
@@ -25,11 +26,13 @@ trail.addComponent(
         ],
     }),
 );
-// init začetni točki za sled
-mesh.vertices.push(new Vertex({ position: [0, 4, -20] }));
-mesh.vertices.push(new Vertex({ position: [0, 1, -20] }));
-mesh.vertices.push(new Vertex({ position: [0, 4, 10] }));
-mesh.vertices.push(new Vertex({ position: [0, 1, 10] }));
+// init začetni točki za sled TO DO: fix inital positions to be single line of starting position
+mesh.vertices.push(new Vertex({ position: [0, 4, 0] }));
+mesh.vertices.push(new Vertex({ position: [0, 1, 0] }));
+mesh.vertices.push(new Vertex({ position: [0, 4, 0] }));
+mesh.vertices.push(new Vertex({ position: [0, 1, 0] }));
+
+let ix = 3;
 
 mesh.indices.push(0, 1, 2);
 mesh.indices.push(1, 2, 3);
@@ -37,12 +40,21 @@ mesh.indices.push(1, 2, 3);
 // dodaj sled za userjem
 trail.addComponent({
     update(t) {
-        console.log(trail.parent.children[2].components[0]);
+        let matrika = trail.parent.children[2].components[0].matrix;
+        let a = [0,0,0]; 
+        let b = [0,0,0];
+        ix = ix + 2;
+        vec3.transformMat4(a, mesh.vertices[0].position, matrika);
+        vec3.transformMat4(b, mesh.vertices[1].position, matrika);
+        
         //dodaj točki
-        //mesh.vertices.push(new Vertex({ position: [7, 6, 10] }));
-        //mesh.vertices.push(new Vertex({ position: [1, 6, 10] }));
+        mesh.vertices.push(new Vertex({ position: a }));
+        mesh.vertices.push(new Vertex({ position: b }));
+        
         // dodaj trikotnika
-        //mesh.indices.push([0, 1, 2]);
-        //mesh.indices.push([1, 2, 3]);
+        mesh.indices.push(ix-3, ix-2, ix-1, ix-2, ix-1, ix);
     },
 });
+
+
+//TO DO: make it 4 point based so it can be seen if in the same plane.
